61ca0bb98de2dfda8763a6359e8e650df67e0e3a
samayaraj2026 <samayaraj.m@mitrahsoft.in>
2026-03-25 13:39:04 +0530

    again passing another arguments

--- api.js
+++ api.js
@@ -5,4 +5,6 @@ function checkRebase(str1,str2){
 console.log(checkRebase("hello","world"))
 console.log(checkRebase("john","mathew"))
 console.log(checkRebase("note","book"))
+console.log(checkRebase("keyboard","mouse"))
+
 
