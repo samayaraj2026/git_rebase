4bc61f1e2111134932e161c2e8eb50ed8a671a5d
samayaraj2026 <samayaraj.m@mitrahsoft.in>
2026-03-26 13:57:08 +0530

    bug fixed

--- api.js
+++ api.js
@@ -2,9 +2,9 @@ function checkRebase(str1,str2){
     return str1+" "+str2;
 }
 
-console.log(checkRebase("hello","world"))
-console.log(checkRebase("john","mathew"))
-console.log(checkRebase("note","book"))
-console.log(checkRebase("keyboard","mouse"))
+console.log(checkRebase("hello","world"));
+console.log(checkRebase("john","mathew"));
+console.log(checkRebase("note","book"));
+console.log(checkRebase("keyboard","mouse"));
 
 
